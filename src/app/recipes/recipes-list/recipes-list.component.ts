271473db5cf1c639494a2529fe1f772e8b1757b3
Bohdan <bohdansternytskyi@gmail.com>
2020-07-15 19:55:15 +0200

    initial commit

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.scss']
+})
+export class RecipesListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'test description', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
